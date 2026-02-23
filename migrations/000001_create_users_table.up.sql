0382b2ac88c0985c1c2936fec6886c2734a616f9
ibrahimsen <m.ibrahimsenn@gmail.com>
2026-02-23 14:53:03 +0200

    created simple database table and migrations

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,6 @@
+CREATE table users (
+id UUID PRIMARY KEY  DEFAULT gen_random_uuid(),
+email VARCHAR(255) UNIQUE NOT NULL,
+password_hash VARCHAR(255) NOT NULL,
+created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
